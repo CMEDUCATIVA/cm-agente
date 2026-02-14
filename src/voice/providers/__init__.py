b77fe5e9032b7990729f23333a0b093024f6f534
CMEDUCATIVA <161378183+CMEDUCATIVA@users.noreply.github.com>
2026-02-14 00:54:42 -0500

    Add ElevenLabsTTS to voice provider exports

--- src/voice/providers/__init__.py
+++ src/voice/providers/__init__.py
@@ -2,9 +2,8 @@
 
 from voice.providers.openai_stt import OpenAISTT
 from voice.providers.openai_tts import OpenAITTS
+from voice.providers.elevenlabs_tts import ElevenLabsTTS
 
 # Future providers can be imported here:
 # from voice.providers.deepgram_stt import DeepgramSTT
-# from voice.providers.elevenlabs_tts import ElevenLabsTTS
-
-__all__ = ["OpenAISTT", "OpenAITTS"]
+__all__ = ["OpenAISTT", "OpenAITTS", "ElevenLabsTTS"]
